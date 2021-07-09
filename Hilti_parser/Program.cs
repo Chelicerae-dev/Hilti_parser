a83ce14393303acac7367ffea74c38bb360420a8
Oleg Bakhmatsky <omg.that.man@gmail.com>
2021-07-09 18:54:32 +0300

    1

---
+++ Hilti_parser/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Office.Interop.Excel;
+
+namespace Hilti_parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            excelArticleGrubber excelGrub = new excelArticleGrubber();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Hilti_parser/excelArticleGrubber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using excel = Microsoft.Office.Interop.Excel;
+
+namespace Hilti_parser
+{
+    public class excelArticleGrubber
+    {
+        excel.Application ex = new excel.Application();
+
+
+
+        public excelArticleGrubber()
+        {
+            Console.WriteLine(ex);
+        }
+    }
+}
