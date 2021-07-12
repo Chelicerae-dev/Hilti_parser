2b4383cfc2944a30cb9867893d44c4a5da4be424
Oleg Bakhmatsky <omg.that.man@gmail.com>
2021-07-12 22:14:32 +0300

    * BaseJson.cs: Working prototype
    * Variants.cs:
    * PriceData.cs:
    * RangePage.cs:
    * PriceStandard.cs:

---
+++ Hilti_parser/PriceData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Hilti_parser
+{
+    public class PriceData
+    {
+        public string materialNumber;
+        public string fleet;
+        public PriceStandard standard;
+        public string monthly;
+        public string yearly;
+        public bool freeProduct;
+
+
+        public PriceData()
+        {
+        }
+    }
+}
---
+++ Hilti_parser/PriceStandard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Hilti_parser
+{
+    public class PriceStandard
+    {
+        public string currencyIso;
+        public decimal value;
+        public string formattedValue;
+        public string quantity;
+        public string priceUnit;
+        public int priceUnitOriginal;
+        public string symbol;
+
+        public PriceStandard()
+        {
+        }
+    }
+}
